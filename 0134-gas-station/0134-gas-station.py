85cc45a9c487d9ff2bd0edc08a84c97b3b039cf2
Pantha Protick <135045897+PanthaProtick@users.noreply.github.com>
2026-07-03 00:37:15 +0600

    Time: 75 ms (5.07%), Space: 21.2 MB (5.03%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,33 +5,26 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        temp=gas
         size=len(gas)
         for i in range(size):
-            temp[i]-=cost[i]
+            gas[i]-=cost[i]
 
-        if sum(temp)>=0:
+        if sum(gas)>=0:
             prefix=[]
             for i in range(size):
                 if i==0:
                     prefix.append(0)
                 else:
-                    prefix.append(prefix[-1]+temp[i-1])
+                    prefix.append(prefix[-1]+gas[i-1])
             i=0
-            while temp[i]<0:
+            while gas[i]<0:
                 i+=1
             k=i+1
             while k<size:
-                if temp[k]>=0:
+                if gas[k]>=0:
                     if prefix[k]-prefix[i]<0:
                         i=k
                 k+=1
             return i
         else:
             return -1
-
-
-# 1 -3 1 -2 3
-# 0 1 -2 -1 -3 0
-# -2 -2 -2 3 3
-# 0 -2 -4 -6 -3
