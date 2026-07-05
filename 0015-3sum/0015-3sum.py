f9772d79cefe55f396ddd0e742b77aa329b37bb7
Pantha Protick <135045897+PanthaProtick@users.noreply.github.com>
2026-07-06 00:11:21 +0600

    Time: 2791 ms (11.49%), Space: 20.4 MB (5.42%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans=set()
+        size=len(nums)
+        nums.sort()
+        for i in range(size-1):
+            self.twoSum(nums[i+1:],nums[i]*(-1),ans)
+        ans2=[]
+        for tup in ans:
+            a,b,c=tup[0],tup[1],tup[2]
+            ans2.append([a,b,c])
+        return ans2
+
+
+    def twoSum(self,nums,target,ans):
+        i=0
+        j=len(nums)-1
+        if i==j:
+            return
+        while i<j:
+            if nums[i]+nums[j]==target:
+                ans.add((target*(-1),nums[i],nums[j]))
+                i+=1
+            elif nums[i]+nums[j]>target:
+                j-=1
+            else:
+                i+=1
+        return ans
