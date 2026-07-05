efb07c359a144784fb52ca408231aba3590d6e8f
Pantha Protick <135045897+PanthaProtick@users.noreply.github.com>
2026-07-06 00:15:32 +0600

    Time: 2789 ms (11.5%), Space: 20.2 MB (7.27%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -9,6 +9,12 @@ class Solution(object):
         nums.sort()
         for i in range(size-1):
             self.twoSum(nums[i+1:],nums[i]*(-1),ans)
+            #target=nums[i]*(-1)
+            #a=i+1
+            #b=size-1
+            #if a==b:
+                #break
+            
         ans2=[]
         for tup in ans:
             a,b,c=tup[0],tup[1],tup[2]
@@ -29,4 +35,4 @@ class Solution(object):
                 j-=1
             else:
                 i+=1
-        return ans
+        return
