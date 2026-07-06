788cc07b9ff6db078ed1ef3681fba5a494b0356d
Pantha Protick <135045897+PanthaProtick@users.noreply.github.com>
2026-07-06 20:39:46 +0600

    Time: 10 ms (30.91%), Space: 12.4 MB (58.81%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,22 @@
-class Solution:
+class Solution(object):
     def isValidSudoku(self, board):
-        rows = [set() for x in range(9)]
-        columns = [set() for x in range(9)]
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        rows=[set() for i in range(9)]
+        cols=[set() for i in range(9)]
         squares = [[set() for x in range(3)] for y in range(3)]
-        
-        for x in range(9):
-            for y in range(9):
-                cell_value = board[x][y]
-                if cell_value == ".":
-                    continue
-                if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
-                    return False
 
-                rows[x].add(cell_value)
-                columns[y].add(cell_value)
-                squares[x//3][y//3].add(cell_value)
-        
+        for i in range(9):
+            for j in range(9):
+                number=board[i][j]
+                if number==".":
+                    continue
+                if number in rows[i] or number in cols[j] or number in squares[i//3][j//3]:
+                    return False
+                
+                rows[i].add(number)
+                cols[j].add(number)
+                squares[i//3][j//3].add(number)
         return True
