9a7eff6554aa726535ac5670ed42963c36f182d9
Pantha Protick <135045897+PanthaProtick@users.noreply.github.com>
2026-08-03 23:41:00 +0600

    Time: 9 ms (30.31%), Space: 12.4 MB (18.47%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        temp=1
+        for i in range(32):
+            if temp>left and temp<=right:
+                return 0
+            if temp>left and temp>right:
+                break
+            temp<<=1
+
+        ans=0
+        for i in range(32):
+            ans|=1
+            ans<<=1
+        ans|=1
+
+        diff=right-left
+        count=0
+        for i in range(32):
+            if diff&1:
+                count=i+1
+            diff>>=1
+
+        ans<<=count   
+        return ans&left&right
