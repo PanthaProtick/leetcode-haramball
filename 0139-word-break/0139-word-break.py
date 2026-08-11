ecf847d165d644a832813afc8bb2719df46ef301
Pantha Protick <135045897+PanthaProtick@users.noreply.github.com>
2026-08-11 23:29:19 +0600

    Time: 69 ms (6.22%), Space: 12.3 MB (73.28%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        size=len(s)
+        arr=[False for i in range(size+1)]
+        arr[0]=True
+        for i in range(1,size+1):
+            idx=i-1
+            for word in wordDict:
+                x=i-len(word)
+                if x>=0 and arr[x] and s[x:idx+1] in wordDict:
+                    arr[i]=True
+        return arr[size]
